63b4ead43cbb9af1d72e36bb5400164725c694ee
castelock <castelock@gmail.com>
2022-09-05 16:41:51 +0200

    Adding new functions to String class.

--- StringFeatures.py
+++ StringFeatures.py
@@ -4,7 +4,7 @@ class StringFeatures:
     def __init__(self, text) -> None:
         self.text = text
 
-    def string_len(self):
+    def stringLen(self):
         print(len(self.text))
 
     def subset(self):
@@ -14,14 +14,35 @@ class StringFeatures:
         print(my_string[:5]) # from start to position 5
         print(my_string[2:]) # from position 2 to the end
     
-    def repeat_strings(self):
+    def repeatStrings(self):
         print('*' * 10)
         print('Hi' * 10)
 
-    def split_strings(self):
+    def splitStrings(self):
         title = 'The Good, the Bad, and the Ugly'
         print('Source string:', title)
         print('Split using a space:')
         print(title.split(' '))
         print('Split using a comma')
         print(title.split(','))
+
+    def countStrings(self):
+        my_string = 'Count, the number    of spaces'
+        print("my_string.count(' '):", my_string.count(' '))
+
+    def replaceStrings(self):
+        welcome_message = 'Hello World!'
+        print(welcome_message.replace("Hello", "Goodbye"))
+    
+    def findSubstrings(self):
+        print('Edward Alun Rawlings'.find('Alun'))
+
+    def convertToString(self):
+        msg = 'Hello Lloyd you are' + str(21)
+        print(msg)
+
+    def otherFunctions(self):
+        some_string = 'Hello World'
+        print('some_string.swapcase()', some_string.swapcase())
+        print('String leading, trailing spaces',"   xyz   ".strip())
+
