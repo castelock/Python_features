f376772630ab9e871f28f93a8bf35dfdd99bcc94
castelock <castelock@gmail.com>
2022-09-07 16:41:58 +0200

    Formatting strings

--- StringFeatures.py
+++ StringFeatures.py
@@ -46,3 +46,30 @@ class StringFeatures:
         print('some_string.swapcase()', some_string.swapcase())
         print('String leading, trailing spaces',"   xyz   ".strip())
 
+    def formatting_string(self):
+        # Allows multiple values to populate the string
+        name = "Adam"
+        age = 20
+        print("{} is {} years old".format(name, age))
+
+        # Can specify an index for the substitution
+        format_string = "Hello {1} {0}, you got {2}%"
+
+        # Can use named substitutions, order is not significant
+        format_string = "{artist} sang {song} in {years}"
+        print(format_string.format(artist= 'Paloma Faith', song = 'Guilty', year = 2017))
+
+        # Can format numbers with comma as thousands separator
+        print('{:,}'.format(1234567890))
+        print('{:,}'.format(1234567890.0))
+
+    def align_strings(self):
+
+        print('|{:25}|'.format('25 characters width'))
+
+        print('|{:<25}|'.format('left aligned'))
+        print('|{:>25}|'.format('right aligned'))
+        print('|{:^25}|'.format('centered'))
+
+
+
